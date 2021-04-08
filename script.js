e6758caad8b1d7215b44c44177733faa4f8d6d59
Hamza Laroussi <HamzaConnection@hotmail.com>
2021-04-08 16:13:32 +0200

    DONE

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger")
 const navUl = document.querySelector(".nav-ul")
+const navLinks = document.querySelectorAll(".nav-ul li a");
 
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show')
@@ -8,4 +9,15 @@ hamburger.addEventListener('click', () => {
 const year = document.getElementById("year")
 year.innerHTML = new Date().getFullYear()
 
+
+navLinks.forEach(element => {
+    element.addEventListener("click", () => {
+
+        if (navUl.classList.contains('show')) {
+            console.log("Hello");
+            navUl.classList.toggle('show')
+        }
+
+    })
+});
 
